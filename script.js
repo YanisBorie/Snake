729305644a40d32f7ec99473010891934e37261c
Yanis Borie <yanis.borie69@gmail.com>
2022-11-14 17:48:12 +0100

    Interface accueil

---
+++ script.js
@@ -0,0 +1,12 @@
+function menuHome() {
+    var facile = document.getElementById("facile");
+    var moyen = document.getElementById("moyen");
+    var difficile = document.getElementById("difficile");
+
+}
+
+function main() {
+    menuHome();
+}
+
+main();
