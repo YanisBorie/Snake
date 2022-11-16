c7d8c0034ecdbd02c176ceba57bf349b4ab0b03b
Yanis Borie <yanis.borie69@gmail.com>
2022-11-16 11:51:25 +0100

    Changement menu

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const btn = document.querySelector('.btn');
-switchBox.addEventListener('click', change);
-
-function menuHome() {
-    var facile = document.getElementById("facile");
-    var moyen = document.getElementById("moyen");
-    var difficile = document.getElementById("difficile");
+function retourMenu() {
+    var menuJeu = document.getElementById("menuJeu");
+    menuJeu.style.display = "none";
+    facile.style.display = "block";
+    moyen.style.display = "block";
+    difficile.style.display = "block";
 
 }
 
 function change() {
-    console.log("Bouton cliqué");
+    var menuJeu = document.getElementById("menuJeu");
+    facile.style.display = "none";
+    moyen.style.display = "none";
+    difficile.style.display = "none";
+
+    menuJeu.style.display = "flex";
 }
 
 function main() {
-    menuHome();
+    var facile = document.getElementById("facile");
+    var moyen = document.getElementById("moyen");
+    var difficile = document.getElementById("difficile");
+    var retour = document.getElementById("retour");
+
+    
+    facile.addEventListener('click', change);
+    moyen.addEventListener('click', change);
+    difficile.addEventListener('click', change);
+    retour.addEventListener('click', retourMenu);
 }
 
 main();
