1f23c44c6d29ca9246b3efdadaf0300beaa37682
BROUTIER CHARLENE p2100510 <charlene.broutier@etu.univ-lyon1.fr>
2022-11-16 09:16:28 +0000

    Replace script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const btn = document.querySelector('.btn');
+switchBox.addEventListener('click', change);
+
 function menuHome() {
     var facile = document.getElementById("facile");
     var moyen = document.getElementById("moyen");
     var difficile = document.getElementById("difficile");
 
+}
+
+function change() {
+    console.log("Bouton cliqué");
 }
 
 function main() {
