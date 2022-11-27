bf1f218c4b39da8b2d1bde1b1a1be00f212246bc
BORIE YANIS p2100107 <yanis.borie@etu.univ-lyon1.fr>
2022-11-27 15:16:17 +0000

    Comptage point + Game Over mur

--- canvas.js
+++ canvas.js
@@ -11,10 +11,14 @@ let world = [
     ['EMPTY', 'EMPTY', 'EMPTY', 'EMPTY', 'EMPTY', 'EMPTY', 'EMPTY', 'EMPTY', 'EMPTY', 'EMPTY'],
   ];
 let snake = [[8,1], [8,2], [8,3]];
+let food = [[1,8]];
+let score = 0;
+var interval;
 var canvas = document.getElementById('mycanvas1');
 var ctx = canvas.getContext('2d');
 let direction;
 
+document.getElementById("reessayer").addEventListener("click", restart);
 
 ctx.fillStyle = "#B97A57"; // Les remplissages
 ctx.fillRect(0,0,canvas.width,canvas.height);
@@ -53,34 +57,45 @@ function creerDamier() {
 }
 
 function move() {
-    if (direction === 'UP') {
-        snake.push([snake[2][0]-1,snake[2][1]]);
-        let oldPosition = snake.shift();
-        world[oldPosition[0]][oldPosition[1]] = "EMPTY";
-        world[snake[2][0]][snake[2][1]] = "SNAKE";
-        creerDamier();
+    if (snake[2][0] == food[0][0] && snake[2][1] == food[0][1]) {
+        console.log("test");
+        score += 1;
+        document.getElementById("score").textContent = "Score : " + score;
+        apparitionPomme();
     }
-    else if (direction === 'LEFT') {
-        snake.push([snake[2][0],snake[2][1]-1]);
-        let oldPosition = snake.shift();
-        world[oldPosition[0]][oldPosition[1]] = "EMPTY";
-        world[snake[2][0]][snake[2][1]] = "SNAKE";
-        creerDamier();
-    }
-    else if (direction === 'RIGHT') {
-        snake.push([snake[2][0],snake[2][1]+1]);
-        let oldPosition = snake.shift();
-        world[oldPosition[0]][oldPosition[1]] = "EMPTY";
-        world[snake[2][0]][snake[2][1]] = "SNAKE";
-        creerDamier();
-    }
-    else if (direction === 'DOWN') {
-        snake.push([snake[2][0]+1,snake[2][1]]);
-        let oldPosition = snake.shift();
-        world[oldPosition[0]][oldPosition[1]] = "EMPTY";
-        world[snake[2][0]][snake[2][1]] = "SNAKE";
-        creerDamier();
-    
+    if (snake[2][0] < 0 || snake[2][0] > 9 || snake[2][1] > 9 || snake[2][1] < 0) {
+        document.getElementById("reessayer").style.display = "block";
+        console.log("GAME OVER");
+        clearInterval(interval);
+    } else {
+        if (direction === 'UP') {
+            snake.push([snake[2][0]-1,snake[2][1]]);
+            let oldPosition = snake.shift();
+            world[oldPosition[0]][oldPosition[1]] = "EMPTY";
+            world[snake[2][0]][snake[2][1]] = "SNAKE";
+            creerDamier();
+        }
+        else if (direction === 'LEFT') {
+            snake.push([snake[2][0],snake[2][1]-1]);
+            let oldPosition = snake.shift();
+            world[oldPosition[0]][oldPosition[1]] = "EMPTY";
+            world[snake[2][0]][snake[2][1]] = "SNAKE";
+            creerDamier();
+        }
+        else if (direction === 'RIGHT') {
+            snake.push([snake[2][0],snake[2][1]+1]);
+            let oldPosition = snake.shift();
+            world[oldPosition[0]][oldPosition[1]] = "EMPTY";
+            world[snake[2][0]][snake[2][1]] = "SNAKE";
+            creerDamier();
+        }
+        else if (direction === 'DOWN') {
+            snake.push([snake[2][0]+1,snake[2][1]]);
+            let oldPosition = snake.shift();
+            world[oldPosition[0]][oldPosition[1]] = "EMPTY";
+            world[snake[2][0]][snake[2][1]] = "SNAKE";
+            creerDamier();
+        }
     }
 }
 
@@ -100,10 +115,40 @@ function step(key) {
     }
 }
 
+function apparitionPomme() {
+    let x=Math.floor(Math.random(11)*10);
+    let y=Math.floor(Math.random(11)*10);
+    if (world[x][y]!="EMPTY") {
+        apparitionPomme();
+    }
+    world[x][y]="FOOD";
+    food[0][0] = x;
+    food[0][1] = y;
+}
+
+function restart() {
+    document.getElementById("reessayer").style.display = "none";
+    for(let i = 0; i < world.length; i++) {
+        for(let j = 0; j < world.length; j++) {
+            world[i][j] = "EMPTY";
+        }
+    }
+    snake = [[8,1], [8,2], [8,3]];
+    world[snake[0][0]][snake[0][1]] = "SNAKE";
+    world[snake[1][0]][snake[1][1]] = "SNAKE";
+    world[snake[2][0]][snake[2][1]] = "SNAKE";
+    food = [[1,8]];
+    world[food[0][0]][food[0][1]] = "FOOD";
+    direction = "";
+    score = 0;
+    document.getElementById("score").textContent = "Score : " + score;
+    main();
+}
+
 function main() {
     creerDamier();
     var key = addEventListener("keydown", step);
-    setInterval(move, 500);
+    interval = setInterval(move, 500);
 }
 
 main();
